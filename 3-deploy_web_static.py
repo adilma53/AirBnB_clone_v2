98970247da3a0e33b0e7b56481cfedabc08e49d2
adil mae <itsadilhadid@gmail.com>
2023-10-09 01:01:20 +0100

    task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
+""" module doc
 """
-deploying a basic web static
-"""
+from fabric.api import task, local, env, put, run, runs_once
 from datetime import datetime
 import os
-from fabric.api import run, env, put, local, task
-env.hosts = ["35.175.63.217", "100.25.194.58"]
+
+env.hosts = ['54.90.63.243', '54.90.4.180']
 
 
-@task
+@runs_once
 def do_pack():
     """
     create a single .tgz file "version" from all the content web_static folder
@@ -30,20 +30,12 @@ def do_deploy(archive_path):
     """
     deploy archive to server
     """
-    if not archive_path or not os.path.exists(archive_path):
-        return False
-
-    put(archive_path, '/tmp')
-
-    ar_name = archive_path[archive_path.find("/") + 1: -4]
     try:
         if not os.path.exists(archive_path):
             return False
-
         fn_with_ext = os.path.basename(archive_path)
         fn_no_ext, ext = os.path.splitext(fn_with_ext)
         dpath = "/data/web_static/releases/"
-
         put(archive_path, "/tmp/")
         run("rm -rf {}{}/".format(dpath, fn_no_ext))
         run("mkdir -p {}{}/".format(dpath, fn_no_ext))
@@ -53,9 +45,18 @@ def do_deploy(archive_path):
         run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
         run("rm -rf /data/web_static/current")
         run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
-
         print("New version deployed!")
         return True
-
     except Exception:
         return False
+
+
+@task
+def deploy():
+    """
+    function to run do_deploy
+    """
+    path = do_pack()
+    if path is None:
+        return False
+    return do_deploy(path)
