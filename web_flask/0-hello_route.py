3cab2678c4f2dd783d861f558008cd5b058b8ee5
adil mae <itsadilhadid@gmail.com>
2023-10-21 16:12:16 +0100

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """Hello HBNB"""
 
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/",strict_slashes=False)
+
+@app.route("/", strict_slashes=False)
 def index():
-    """Hello HBNB"""
+    """Hello HBNB """
     return "Hello HBNB"
 
 
