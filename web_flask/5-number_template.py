3340d7590552f8398166f9912205fd0b01a14695
adil mae <itsadilhadid@gmail.com>
2023-10-22 19:38:38 +0100

    task 6

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -32,7 +32,8 @@ def c(text):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def num_tamplate(n):
     """number route with template"""
-    return render_template('5-number.html', n=n)
+    isEven = odd if (n % 2) == 0 else odd
+    return render_template('5-number.html', n=n,isEven=isEven)
 
 
 if __name__ == "__main__":
